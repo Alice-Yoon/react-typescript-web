5dadcbefb823c885da4811dc70103443d9bcfca6
윤지혜 <yoonjihye@yunjihyeui-MacBook-Pro.local>
2022-03-03 23:23:43 +0900

    [feat]: Navigation 컴포넌트 생성

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav>
+      <h1>Caffein</h1>
+      <a>
+        Icon
+        {/* Icon */}
+      </a>
+    </nav>
+  )
+}
+
+export default Navigation
