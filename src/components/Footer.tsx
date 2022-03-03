282a1c3a6d34f3cf44bafc7280f625619aed6b5e
윤지혜 <yoonjihye@yunjihyeui-MacBook-Pro.local>
2022-03-03 23:23:28 +0900

    [feat]: Footer 컴포넌트 생성

---
+++ src/components/Footer.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <h1>caffein</h1>
+      <div>
+        <dl>
+          <div>
+            <dt>대표:</dt>
+            <dd>나대표</dd>
+          </div>
+          <div>
+            <dt>주소:</dt>
+            <dd>서울특별시 중구 한강대로 416, 서울스퀘어 15층 101호</dd>
+          </div>
+          <div>
+            <dt>저작권:</dt>
+            <dd>Copyright by (주)카페인. All right reserved.</dd>
+          </div>
+        </dl>
+        <a>이용약관</a>
+        <a>개인정보처리방침</a>
+      </div>
+      <div>
+        <img src="" alt="logo1"/>
+        <img src="" alt="logo2"/>
+        <img src="" alt="logo3"/>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
