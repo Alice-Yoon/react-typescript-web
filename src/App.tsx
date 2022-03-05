7708de428d00090ceeefc344b6652548bf61a4d1
윤지혜 <yoonjihye@yunjihyeui-MacBook-Pro.local>
2022-03-05 20:57:10 +0900

    [feat]: index.tsx & App.tsx에 react-router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { CampApply, CampDetail, Home } from './pages';
 
 function App() {
   return (
-    <div>
-      My React Typescript Web Project
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/campDetail/:id" element={<CampDetail />} />
+      <Route path="/campApply" element={<CampApply />} />
+    </Routes>
   );
 }
 
