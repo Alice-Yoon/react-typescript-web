4aac2f5550309473d60071395a423679b27d6551
윤지혜 <yoonjihye@yunjihyeui-MacBook-Pro.local>
2022-03-05 20:07:12 +0900

    [feat]: Home화면 css , image 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,48 +1,66 @@
 import React, { Fragment } from 'react'
+import Styles from './Home.module.css'
+
 import CampCard from '../../components/CampCard'
 import CommunityCard from '../../components/CommunityCard'
 import Footer from '../../components/Footer'
 import Navigation from '../../components/Navigation'
 
+import homeHeaderImage from '../../assets/images/home_header_image.png'
+
 const Home = () => {
   return (
     <>
-      <header>
-        <Navigation />
-        <div>
-          <h2>개발은 카페인과 함께</h2>
-          <img src="" alt=""/>
+      <header className={Styles.header}>
+        <div className={Styles.container}>
+          <Navigation />
+          <div className={Styles.headerContents}>
+            <h2 className={Styles.headerTitle}>
+              개발은 <br />카페인과 함께
+            </h2>
+            <img 
+              className={Styles.headerImage} 
+              src={homeHeaderImage} 
+              alt={homeHeaderImage} 
+            />
+          </div>
         </div>
       </header>
 
-      <main>
+      <main className={Styles.container}>
         {/* 캠프 리스트 */}
         <section>
-          { DUMMY_CAMPDATA && DUMMY_CAMPDATA.map(data => (
-              <Fragment key={data.type.value}>
-                <h1>{data.type.text}</h1>
-                <ul>
+          { 
+            DUMMY_CAMPDATA && DUMMY_CAMPDATA.map(data => (
+              <div key={data.type.value}>
+                <h1 className={Styles.cardListTitle}>{data.type.text}</h1>
+                <ul className={Styles.cardList}>
                   {data.camps.map(camp => (
                     <li key={camp.id}>
                       <CampCard contentData={camp} />
                     </li>
                   ))}
                 </ul>
-              </Fragment>
-            ))}
+              </div>
+            ))
+          }
         </section>
 
-        {/* 메세지 */}
-        <div>
+        {/* 배너 */}
+        <div className={Styles.banner}>
           <p>현직자와 소통하며 배우는 실무 스킬, 퍼스널 트레이닝</p>
         </div>
 
         {/* 커뮤니티 리스트 */}
         <section>
           <h1>커뮤니티</h1>
-          { DUMMY_COMMUNITYDATA && DUMMY_COMMUNITYDATA.map((data => (
-            <CommunityCard key={data.id} contentData={data} />  
-          )))}
+          <div className={Styles.cardList}>
+          { 
+            DUMMY_COMMUNITYDATA && DUMMY_COMMUNITYDATA.map((data => (
+              <CommunityCard key={data.id}  contentData={data} />  
+            )))
+          }
+          </div>
         </section>
       </main>
 
